e38968e3c4d29e0aab730c145d3130fd76fa2491
sihamais <sihamais98@gmail.com>
2022-06-26 23:19:19 +0200

    feat(init): add env config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16
+FROM node:latest
 WORKDIR /
 COPY package.json .
 RUN npm install
--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ AppDataSource.initialize()
     );
 
     console.log(
-      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
+      `Express server has started on port ${port}. Open http://localhost:${port}/users to see results`
     );
   })
   .catch((error) => console.log(error));
