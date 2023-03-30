364587a10f60e1bfe7f1f76e2f5ee1eac4da9a56
sihamais <sihamais98@gmail.com>
2023-03-30 23:39:36 +0200

    fix: deploy

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import { INestMicroservice, ValidationPipe } from '@nestjs/common';
+import {
+  INestApplication,
+  INestMicroservice,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
@@ -7,21 +11,19 @@ import { protobufPackage } from './auth/auth.pb';
 // import { HttpExceptionFilter } from './auth/filter/http-exception.filter';
 
 async function bootstrap() {
-  const app: INestMicroservice = await NestFactory.createMicroservice(
-    AppModule,
-    {
-      transport: Transport.GRPC,
-      options: {
-        url: `${process.env.URL}:${process.env.PORT}`,
-        package: protobufPackage,
-        protoPath: join('node_modules/proto/proto/auth.proto'),
-      },
+  const app: INestApplication = await NestFactory.create(AppModule);
+  app.connectMicroservice({
+    transport: Transport.GRPC,
+    options: {
+      url: `${process.env.URL}:${process.env.PORT}`,
+      package: protobufPackage,
+      protoPath: join('node_modules/proto/proto/auth.proto'),
     },
-  );
+  });
 
   // app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.listen();
+  app.listen(+process.env.PORT || 3000);
 
   console.log(
     `Application is running on: ${process.env.URL}:${process.env.PORT}`,
